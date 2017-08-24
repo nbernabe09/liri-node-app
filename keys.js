67b87daed2d752be8dd6c781e7fe931b577b4130
Nathan Bernabe <nbernabe09@aol.com>
2017-08-23 23:04:03 -0700

    Adding offline files, missing command do-what-it-says

---
+++ keys.js
@@ -0,0 +1,17 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'w3qxJZgspUkCZOs3UY3tfvpiP',
+  consumer_secret: 'vkPDLqv531M8oeb2zz3AcU4yLHt0wwgI8zSRVShJUiPuV8WTpQ',
+  access_token_key: '900575417139986432-PQMvuBDqQz1jUGrfpFpPRzSe0t0JFMe',
+  access_token_secret: 'ymxGLIGKrFQtmSuoMbhc2hMDdTgE6yVc8r8iF2dEzhPD5',
+};
+
+exports.spotifyKeys = {
+	id: 'ac55941514594ddf9c35c559567ffb68',
+	secret: 'e5ec0c0230f94e14a610e8bf326d2cf4'
+};
+
+exports.omdbKey = {
+	api: '40e9cece'
+}
